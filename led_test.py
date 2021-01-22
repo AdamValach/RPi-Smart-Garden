c958738fa00eb4c144ebf5551d292167becfd41c
Adam Valach <43640500+AdamValach@users.noreply.github.com>
2021-01-22 15:01:12 +0100

    feedback

--- led_test.py
+++ led_test.py
@@ -5,7 +5,10 @@ from time import sleep
 ziarovka = LED(18)
 
 while True:
+    print("Starting")
     ziarovka.on()
+    print("Bulb on")
     sleep(1)
     ziarovka.off()
+    print("Bulb off")
     sleep(1)
